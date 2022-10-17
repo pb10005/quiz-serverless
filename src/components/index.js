13b96ef815a0b0028b67f4930eb81aa1f30c0440
pb10005 <atihsa.mihs.ne.ho.da@gmail.com>
2022-10-18 00:24:40 +0900

    Update layout

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import PlayerList from './PlayerList.vue'
 import QuizStatus from './QuizStatus.vue'
 import RoomChat from './RoomChat.vue'
 import RoomInfo from './RoomInfo.vue'
+import RoomCard from './RoomCard.vue'
 
 export {
     PlayerList,
     QuizStatus,
+    RoomCard,
     RoomChat,
-    RoomInfo
+    RoomInfo,
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@ import { useRouter } from 'vue-router'
 import { store } from '../store'
 import client from '../supabase-client'
 import { supabase } from '../supabase'
+import { RoomCard } from '../components'
 
 const state = reactive({
   newRoom: {
@@ -54,69 +55,84 @@ onUnmounted(() => {
 
 <template>
   <div class="grid md:grid-cols-12">
-    <div class="md:col-start-1 md:col-span-3 p-2 h-auto">
-      <form @submit.prevent="createRoom" class="bg-white shadow p-2">
-        <div>
-          <label class="font-bold">出題する</label>
-          <input type="text" v-model="state.newRoom.title" class="px-3 py-2 h-10 border-0 border-b-2 border-indigo-700 w-full mb-2" placeholder="部屋名" required/>
-          <br>
-          <input id="public" type="checkbox" v-model="state.newRoom.is_public" checked class="mr-1" />
-          <label for="public">ホーム画面の一覧に表示する</label>
-          <br>
-          <button type="submit" class="rounded border-0 bg-indigo-200 hover:bg-indigo-700 hover:text-white ease-in-out duration-300 px-3 py-1 w-full">部屋を作成する</button>
-        </div>
-      </form>
-    </div>
-    <div class="md:col-start-4 md:col-span-9 p-2 mt-2">
+    <div class="md:col-start-3 md:col-span-8 p-2 mt-2">
+      <div class="p-2 h-auto">
+        <form @submit.prevent="createRoom" class="bg-white shadow p-2">
+          <div>
+            <div class="font-bold text-lg my-2">出題する</div>
+            <input type="text" v-model="state.newRoom.title" class="px-3 py-2 h-10 border-0 border-b-2 border-indigo-700 w-full mb-2" placeholder="部屋名" required/>
+            <div class="my-2">
+              <input id="public" type="checkbox" v-model="state.newRoom.is_public" checked class="mr-1" />
+              <label for="public">ホーム画面の一覧に表示する</label>
+            </div>
+            <button type="submit" class="rounded border-0 bg-indigo-200 hover:bg-indigo-700 hover:text-white ease-in-out duration-300 px-3 py-1 w-full">部屋を作成する</button>
+          </div>
+        </form>
+      </div>
+      <div class="text-lg font-bold my-4">AIと遊ぼう</div>
       <div class="grid md:grid-cols-12">
-        <div class="bg-white shadow p-2 md:col-span-4">
-          <div class="font-bold text-lg mb-1">101倍のお金</div>
-          <div class="text-gray-700 mb-1">管理者</div>
-          <div class="p-2">
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">AI</span>
+        <div class="md:col-span-4 p-2">
+          <div class="bg-white shadow p-2">
+            <div class="font-bold text-lg mb-1">101倍のお金</div>
+            <div class="text-gray-700 mb-1">管理者</div>
+            <div class="p-2">
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">AI</span>
+            </div>
+            <span class="">
+              <router-link class="rounded border-0 bg-indigo-200 hover:bg-indigo-700 hover:text-white ease-in-out duration-300 px-3 py-1" :to="`/quiz/play-with-ai/?id=1`">参加(ログイン不要)</router-link>
+            </span>
           </div>
-          <span class="">
-            <router-link class="rounded border-0 bg-indigo-200 hover:bg-indigo-700 hover:text-white ease-in-out duration-300 px-3 py-1" :to="`/quiz/play-with-ai/?id=1`">遊ぶ(ログイン不要)</router-link>
-          </span>
         </div>
-        <div class="bg-white shadow p-2 md:col-span-4">
-          <div class="font-bold text-lg mb-1">望月家のしきたり</div>
-          <div class="text-gray-700 mb-1">管理者</div>
-          <div class="p-2">
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">AI</span>
+        <div class="p-2 md:col-span-4">
+          <div class="bg-white shadow p-2">
+            <div class="font-bold text-lg mb-1">望月家のしきたり</div>
+            <div class="text-gray-700 mb-1">管理者</div>
+            <div class="p-2">
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">AI</span>
+            </div>
+            <span class="">
+              <router-link class="rounded border-0 bg-indigo-200 hover:bg-indigo-700 hover:text-white ease-in-out duration-300 px-3 py-1" :to="`/quiz/play-with-ai/?id=2`">参加(ログイン不要)</router-link>
+            </span>
           </div>
-          <span class="">
-            <router-link class="rounded border-0 bg-indigo-200 hover:bg-indigo-700 hover:text-white ease-in-out duration-300 px-3 py-1" :to="`/quiz/play-with-ai/?id=2`">遊ぶ(ログイン不要)</router-link>
-          </span>
         </div>
       </div>
-      <div class="text-lg font-bold">参加者募集中の部屋</div>
-      <div class="divide-y">
-        <div v-for="item in state.rooms" :key="item.id" class="flex justify-between p-2">
-          <span class="order-first">{{ item.title }} by {{ item.players.player_name}}</span>
-          <span class="order-last">
-            <router-link class="order-last rounded border-0 bg-indigo-200 hover:bg-indigo-700 hover:text-white ease-in-out duration-300 px-3 py-1" :to="`/quiz/master/${item.id}`" v-if="store.user?.id === item.master_id">出題者画面</router-link>
-            <router-link class="order-last rounded border-0 bg-indigo-200 hover:bg-indigo-700 hover:text-white ease-in-out duration-300 px-3 py-1" :to="`/quiz/play/${item.id}`" v-else>参加</router-link>
-          </span>
+      <div class="text-lg font-bold my-4">参加者募集中の部屋</div>
+      <div class="grid md:grid-cols-12">
+        <div v-for="item in state.rooms" :key="item.id" class="md:col-span-4 p-2">
+          <room-card
+            class=""
+            :title="item.title"
+            :owner="item.players.player_name"
+            :tags="['ウミガメのスープ']"
+            :roomId="item.id"
+            :isMaster="store.user?.id === item.master_id">
+          </room-card>
+         </div>
+      </div>
+      <div class="text-lg font-bold my-4">あなたが参加中の部屋</div>
+      <div class="grid md:grid-cols-12">
+        <div v-for="item in state.participatingRooms" :key="item.id" class="md:col-span-4 p-2">
+          <room-card
+            class=""
+            :title="item.title"
+            :owner="item.players.player_name"
+            :tags="['ウミガメのスープ']"
+            :roomId="item.id"
+            :isMaster="store.user?.id === item.master_id">
+          </room-card>
         </div>
       </div>
-      <div class="text-lg font-bold">あなたが参加中の部屋</div>
-      <div class="divide-y">
-        <div v-for="item in state.participatingRooms" :key="item.id" class="flex justify-between p-2">
-          <span class="order-start">{{ item.rooms.title }} by {{ item.rooms.players.player_name }}</span>
-          <span class="order-last align-center space-x-1">
-            <router-link class="rounded border-0 bg-indigo-200 hover:bg-indigo-700 hover:text-white ease-in-out duration-300 px-3 py-1" :to="`/quiz/play/${item.room_id}`">入室</router-link>
-          </span>
-        </div>
-      </div>
-      <div class="text-lg font-bold">あなたが作成した部屋</div>
-      <div class="divide-y">
-        <div v-for="item in state.ownRooms" :key="item.id" class="flex justify-between p-2">
-          <span class="order-start">{{ item.title }}</span>
-          <span class="order-last align-center space-x-1">
-            <router-link class="rounded border-0 bg-indigo-200 hover:bg-indigo-700 hover:text-white ease-in-out duration-300 px-3 py-1" :to="`/quiz/master/${item.id}`">出題者画面</router-link>
-            <router-link class="rounded border-0 bg-indigo-200 hover:bg-indigo-700 hover:text-white ease-in-out duration-300 px-3 py-1" :to="`/quiz/play/${item.id}`">参加者画面</router-link>
-          </span>
+      <div class="text-lg font-bold my-4">あなたが作成した部屋</div>
+      <div class="grid md:grid-cols-12">
+        <div v-for="item in state.ownRooms" :key="item.id" class="md:col-span-4 p-2">
+          <room-card
+            class=""
+            :title="item.title"
+            :owner="item.players.player_name"
+            :tags="['ウミガメのスープ']"
+            :roomId="item.id"
+            :isMaster="store.user?.id === item.master_id">
+          </room-card>
         </div>
       </div>
     </div>
