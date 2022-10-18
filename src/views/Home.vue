9267cb4b04c0aab2407f22b4aeaf28975975768a
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2022-10-18 14:51:50 +0000

    Fix a bug

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -115,7 +115,7 @@ onUnmounted(() => {
           <room-card
             class=""
             :title="item.rooms.title"
-            :owner="item.rooms.player_name"
+            :owner="item.rooms.players.player_name"
             :tags="['ウミガメのスープ']"
             :roomId="item.rooms.id"
             :isMaster="store.user?.id === item.master_id">
