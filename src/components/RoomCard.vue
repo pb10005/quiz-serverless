bd26a0df160df60b30c9c51b7133fe082c6a5103
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2022-10-22 10:45:45 +0000

    Update layout

--- src/components/RoomCard.vue
+++ src/components/RoomCard.vue
@@ -25,7 +25,7 @@ defineProps({
 </script>
 <template>
     <div class="bg-white rounded-lg shadow-xl p-2">
-        <div class="font-bold text-lg mb-1">{{title}}</div>
+        <div class="font-bold text-lg my-4">{{title}}</div>
         <div class="text-gray-700 mb-1">{{owner}}</div>
         <div class="p-2">
             <span v-for="(item, index) in tags" :key="index" class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{{item}}</span>
