f8b10213bebdd72e1ccc2b34ef4bf257b7489c68
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2022-10-22 15:12:56 +0000

    Implement editing bio

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -8,13 +8,14 @@ import { ActionButton } from '../components'
 const router = useRouter()
 
 const playerName = ref("")
+const bio = ref("")
 const registeredName = ref("")
 
 const setUsername = async () => {
     const { error } = await client.upsertProfile({
         id: store.user.id,
         player_name: playerName.value,
-        bio: 'None'
+        bio: bio.value
     })
     if(!error) {
         router.push("/")
@@ -22,9 +23,10 @@ const setUsername = async () => {
 }
 
 const getUsername = async () => {
-    const username = await client.getUsername()
-    registeredName.value = username
-    playerName.value = username
+    const profile = await client.getProfile()
+    registeredName.value = profile.playerName
+    playerName.value = profile.playerName
+    bio.value = profile.bio
 }
 
 onMounted(async () => {
@@ -34,10 +36,11 @@ onMounted(async () => {
 </script>
 <template>
     <div class="p-2">
-        <div v-if="registeredName">ユーザー名を変更</div>
+        <div v-if="registeredName">プロフィールを更新</div>
         <div v-else>ユーザー名を登録してはじめて出題、参加することができます</div>
         <form @submit.prevent="setUsername">
-            <input type="text" v-model="playerName" class="px-4 py-2 mr-1 h-10 border-0 border-b-2 border-indigo-700" placeholder="ユーザー名" required />
+            <input type="text" v-model="playerName" class="px-4 py-2 mb-2 mr-1 h-10 border-0 border-b-2 border-indigo-700" placeholder="ユーザー名" required /><br>
+            <input type="text" v-model="bio" class="px-4 py-2 mb-2 mr-1 h-10 border-0 border-b-2 border-indigo-700" placeholder="自己紹介" /><br>
             <action-button label="登録"></action-button>
         </form>
     </div>
