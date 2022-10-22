1cf60c6db0b9aa645836fdc957dc1e2898b6dfb7
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2022-10-22 16:38:51 +0000

    Update layout

--- src/components/ProfileTab.vue
+++ src/components/ProfileTab.vue
@@ -32,9 +32,9 @@ defineProps(
             <div class="overflow-hidden relative w-10 h-10 bg-gray-100 rounded-full dark:bg-gray-600 mr-2">
                 <svg class="absolute -left-1 w-12 h-12 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
             </div>
-            <span class="text-xl my-2">
-                <div>{{player.playerName}}</div>
-            </span>
+            <div class="text-xl font-extrabold my-2">
+                {{player.playerName}}
+            </div>
         </div>
         <div class="p-4">{{player.bio}}</div>
         <link-button to="/profile" label="編集"></link-button>
