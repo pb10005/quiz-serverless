51aae20c3b54b8ae07b2e0b8bf15943c1ef49045
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2022-10-22 10:11:36 +0000

    Update layout

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -3,6 +3,7 @@ import { ref, reactive, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import { store } from '../store'
 import client from '../supabase-client'
+import { ActionButton } from '../components'
 
 const router = useRouter()
 
@@ -35,8 +36,8 @@ onMounted(async () => {
         <div v-if="registeredName">ユーザー名を変更</div>
         <div v-else>ユーザー名を登録してはじめて出題、参加することができます</div>
         <form @submit.prevent="setUsername">
-            <input type="text" v-model="playerName" class="px-4 py-2 h-10 border-0 border-b-2 border-indigo-700" placeholder="ユーザー名" required />
-            <button class="rounded border-0 bg-indigo-200 hover:bg-indigo-700 hover:text-white ease-in-out duration-300 ml-2 px-3 py-1">登録</button>
+            <input type="text" v-model="playerName" class="px-4 py-2 mr-1 h-10 border-0 border-b-2 border-indigo-700" placeholder="ユーザー名" required />
+            <action-button label="登録"></action-button>
         </form>
     </div>
 </template>
