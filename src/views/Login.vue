0b6abc3d266e2d3d1661735d0b0c70d4dbd0a1d1
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2022-11-13 09:06:54 +0000

    Update layout

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,7 +3,7 @@ import { ref, reactive, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import { store } from '../store'
 import client from '../supabase-client'
-import { ActionButton } from '../components'
+import { ActionButton, LinkButton } from '../components'
 
 const router = useRouter()
 
@@ -20,8 +20,9 @@ const signIn = async () => {
 }
 </script>
 <template>
-  <div class="md:grid md:grid-cols-3">
-    <form v-if="!store.user?.id" @submit.prevent="signIn" class="md:col-start-2 md:col-span-1 p-2 rounded bg-white shadow mt-2">
+  <div class="md:grid md:grid-cols-3 md:grid-rows-3 w-full min-h-screen">
+    <form v-if="!store.user?.id" @submit.prevent="signIn" class="md:col-start-2 md:col-span-1 px-2 py-4 rounded-xl bg-white shadow-xl mt-2">
+      <div class="font-bold text-lg my-1">ログイン</div>
       <div class="mb-2">
           <input type="email" v-model="email" class="px-4 py-2 h-10 border-0 border-b-2 border-indigo-700 w-full" placeholder="メールアドレス" required/>
       </div>
@@ -32,10 +33,12 @@ const signIn = async () => {
           <action-button label="ログイン"></action-button>
           <div>または<router-link to="/signup" class="underline">ユーザー登録</router-link></div>
       </div>
-  </form>
-    <div v-else>
-        <div>ログイン済です</div>
-        <router-link to="/" class="rounded border-solid border-2 ml-2 px-3 py-1">ホーム画面へ</router-link>
+    </form>
+    <div v-else class="md:col-start-2 md:col-span-1 md:row-start-2 md:row-span-1">
+      <div>
+        <p class="text-center font-bold text-xl my-4">ログイン済です</p>
+        <link-button class="w-full text-center" to="/" label="ホーム画面へ"></link-button>
+      </div>
     </div>
   </div>
 </template>
